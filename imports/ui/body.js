4f906c3bb837842361026ce1933685913e633d58
cct3000 <cct3000@gmail.com>
2016-07-17 12:38:07 +0800

    finisded completed temporary ui state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,26 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
  
 Template.body.helpers({
   tasks() {
+  	const instance = Template.instance();
+  	if (instance.state.get('hideCompleted')) {
+  		return Tasks.find({ checked: { $ne: true } }, {sort: { createdAt: -1} } );
+  	}
   	// Show newest tasks at the top
   	return Tasks.find({}, { sort: { createdAt: -1} } );
+  },
+  incompleteCount() {
+  	return Tasks.find({ checked: { $ne: true } } ).count();
   },
 });
 
@@ -30,6 +42,10 @@ Template.body.events({
     // Clear form
     target.text.value = '';
 
-    console.log(event)
+    //console.log(event)
   },
+  'change .hide-completed input' (event, instance) {
+  	instance.state.set('hideCompleted', event.target.checked);
+  },
+
 });
